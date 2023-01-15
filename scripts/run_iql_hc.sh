28ad8d63d35330058909077bb1e2cf8d8acca66f
typoverflow <typoverflow@outlook.com>
2023-01-15 17:58:46 +0800

    benchmark iql

---
+++ scripts/run_iql_hc.sh
@@ -0,0 +1,23 @@
+export PYTHONPATH=".":${PATHONPATH}
+all_domain=(
+    "halfcheetah"
+    # "hopper"
+    # "walker2d"
+)
+
+all_quality=(
+    "medium"
+    "medium-replay"
+    "medium-expert"
+    "random"
+)
+
+seed=$1
+
+for task in ${all_domain[@]}; do
+    for q in ${all_quality[@]}; do
+        echo python3 run_example/run_iql.py --task ${task}-${q}-v2 --seed ${seed}
+        python3 run_example/run_iql.py --task ${task}-${q}-v2 --seed ${seed}
+    done
+done
+
